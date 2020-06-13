f40171ff519ee97ac1d5c4ef653baa90dff70218
Hedy Li <hedyhyry@gmail.com>
2020-06-13 20:49:49 +0800

    update table width in starcli/utils.py table_output()

--- starcli/utils.py
+++ starcli/utils.py
@@ -39,18 +39,18 @@ def table_output(repos):
     table = Table()
 
     table.add_column("Name", style="bold cyan", no_wrap=True, width=30)
-    table.add_column("Language", style="green", no_wrap=True, width=15)
-    table.add_column("Description", style="blue", no_wrap=False, width=78)
-    table.add_column("Stats", style="magenta", no_wrap=True, width=150)
+    table.add_column("Language", style="green", no_wrap=True, width=23)
+    table.add_column("Description", style="blue", no_wrap=False, width=150)
+    table.add_column("Stats", style="magenta", no_wrap=True, width=75)
 
     for repo in repos:
-        stats = str(repo['stargazers_count']) + " Stars," + \
-            str(repo['forks_count']) + " Forks," + \
-            str(repo['watchers_count']) + "Watchers"
+        stats = str(repo['stargazers_count']) + " Stars, " + \
+            str(repo['forks_count']) + " Forks, " + \
+            str(repo['watchers_count']) + " Watchers"
 
         table.add_row(
                 str(repo["name"]) + "\n",
-                str(repo["language"]),
+                str(repo["language"] + "\n"),
                 str(repo["description"]),
                 stats
             )
