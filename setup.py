a3fe86e77aad35c6b6e4eb416dbea6931702aea7
Hedy Li <hedyhyry@gmail.com>
2020-06-21 12:25:27 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with io.open("pypi_desc.md", "rt", encoding="utf8") as f:
 # This call to setup() does all the work
 setup(
     name="starcli",
-    version="1.2.2",
+    version="1.3.0",
     description="Browse popular repos on github by star trends from your command line!",
     long_description=long_desc,
     python_requires=">=3.7",
