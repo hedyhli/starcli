498fc236388caf4093df0924d2915e2c000f952a
Hedy Li <hedyhyry@gmail.com>
2020-06-23 07:56:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,9 +28,6 @@ setup(
     ],
     packages=["starcli"],
     include_package_data=True,
-    install_requires=[
-        "requests>=2.22.0",
-        "rich>=2.1.0",
-    ],
+    install_requires=["requests>=2.22.0", "rich>=2.1.0",],
     entry_points={"console_scripts": ["starcli=starcli.__main__:cli",]},
 )
