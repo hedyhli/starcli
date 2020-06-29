ffc629999244e3e2fac012104d18517e98272899
Hedy Li <hedyhyry@gmail.com>
2020-06-29 11:02:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 with io.open("README.md", "rt", encoding="utf8") as f:
     long_desc = f.read()
 
-VERSION = "2.6.0"
+VERSION = "2.6.1"
 
 # This call to setup() does all the work
 setup(
