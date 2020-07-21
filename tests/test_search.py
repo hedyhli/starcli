efcbefdb214eedbc56c5a0dbb57d3620eafe4bb3
arcanearronax <arcanearronax@gmail.com>
2020-07-21 00:39:07 -0400

    Modified the date related tests to only validate the created date is at least the provided date. The dates are also randomly generated, rather than preset.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,6 +1,7 @@
 """ tests.test_search """
 
 from datetime import datetime, timedelta
+from random import randint
 from starcli.search import search
 
 
@@ -34,7 +35,7 @@ def test_search_topics():
     """
     Test the search functionality for starcli.search.
     """
-    repos = search(topics=["open", "source"])
+    repos = search(language="python", topics=["open", "source"])
     for repo in repos:
         assert repo["stargazers_count"] >= 0
         assert repo["watchers_count"] >= 0
@@ -48,9 +49,12 @@ def test_search_created_date():
     """
     Test the search functionality for starcli.search.
     """
-    created_date_value = "2020-01-01"
     date_format = "%Y-%m-%d"
-    repos = search(date_created=created_date_value)
+    day_range = 0 - randint(100, 400)
+    created_date_value = (datetime.utcnow() + timedelta(days=day_range)).strftime(
+        date_format
+    )
+    repos = search(language="python", date_created=created_date_value)
     for repo in repos:
         assert repo["stargazers_count"] >= 0
         assert repo["watchers_count"] >= 0
@@ -58,25 +62,26 @@ def test_search_created_date():
         assert (repo["description"] is None) or repo["description"]
         assert repo["full_name"].count("/") >= 1
         assert repo["html_url"] == "https://github.com/" + repo["full_name"]
+
         # Need to account for min and max created dates
-        assert datetime.strptime(
-            repo["created_at"].split("T")[0], date_format
-        ) >= datetime.strptime(created_date_value, date_format) and datetime.strptime(
-            repo["created_at"].split("T")[0], date_format
-        ) <= datetime.strptime(
-            created_date_value, date_format
-        ) + timedelta(
-            days=1
+        repo_created_date = datetime.strptime(
+            repo["created_at"].split("T")[0],
+            date_format,  # Need to remove the time portion to avoid errors
         )
+
+        assert repo_created_date >= datetime.strptime(created_date_value, date_format)
 
 
 def test_search_updated_date():
     """
     Test the search functionality for starcli.search.
     """
-    updated_date_value = "2020-01-01"
     date_format = "%Y-%m-%d"
-    repos = search(last_updated=updated_date_value)
+    day_range = 0 - randint(100, 400)
+    updated_date_value = (datetime.utcnow() + timedelta(days=day_range)).strftime(
+        date_format
+    )
+    repos = search(language="python", last_updated=updated_date_value)
     for repo in repos:
         assert repo["stargazers_count"] >= 0
         assert repo["watchers_count"] >= 0
@@ -84,23 +89,21 @@ def test_search_updated_date():
         assert (repo["description"] is None) or repo["description"]
         assert repo["full_name"].count("/") >= 1
         assert repo["html_url"] == "https://github.com/" + repo["full_name"]
+
         # Need to account for min and max updated dates
-        assert datetime.strptime(
-            repo["updated_at"].split("T")[0], date_format
-        ) >= datetime.strptime(updated_date_value, date_format) and datetime.strptime(
-            repo["updated_at"].split("T")[0], date_format
-        ) <= datetime.strptime(
-            updated_date_value, date_format
-        ) + timedelta(
-            days=1
+        repo_updated_date = datetime.strptime(
+            repo["updated_at"].split("T")[0],
+            date_format,  # Need to remove the time portion to avoid errors
         )
+
+        assert repo_updated_date >= datetime.strptime(updated_date_value, date_format)
 
 
 def test_search_stars():
     """
     Test the search functionality for starcli.search.
     """
-    repos = search(stars="1")
+    repos = search(language="python", stars="1")
     for repo in repos:
         assert repo["stargazers_count"] == 1
         assert repo["watchers_count"] >= 0
