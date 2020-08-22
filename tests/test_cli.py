42d377b5efb07bcc1cb51acafe75341849284597
Hedy Li <hedyhyry@gmail.com>
2020-08-22 00:01:51 +0000

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -246,7 +246,7 @@ class TestCli:
         if exit_code:
             assert result.exit_code == exit_code, f"`exit_code` should be '{exit_code}'"
         if debug:
-            assert "DEBUG:" in result.output, f"'DEBUG' not in `result.output`"
+            assert "DEBUG" in result.output, f"'DEBUG' not in `result.output`"
         if output:
             assert result.output, "No cli output generated"
         elif not output:
@@ -260,6 +260,6 @@ class TestCli:
         if not_in_output:
             for s in not_in_output:
                 assert (
-                    False if re.search(s, result.output) else True,
+                    not re.search(s, result.output),
                     f"{not_in_output} found in `result.output`, but shouldn't be.",
                 )
