d888d599e045f3c803c51e430c6afbd82cc37e1b
Hedy Li <hedyhyry@gmail.com>
2020-09-30 16:15:27 +0800

    remove check debug logs in test_cli assertions()

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -245,8 +245,8 @@ class TestCli:
         """
         if exit_code:
             assert result.exit_code == exit_code, f"`exit_code` should be '{exit_code}'"
-        if debug:
-            assert "" in result.output, f"'DEBUG' not in `result.output`"
+        # if debug: # logs arent captured in result.output so it doesnt work
+            # assert "DEBUG" in result.output, f"'DEBUG' not in `result.output`"
         if output and not debug:
             assert result.output, "No cli output generated"
         elif not output:
